4f5fd539ea3a3dd7e4b5b3847bd1f2962b703586
Aditya Kumar Rai <rai.aditya2k@gmail.com>
2023-01-06 23:05:57 +0530

    multi dimentional Array

--- src/Arrays_Playground.java
+++ src/Arrays_Playground.java
@@ -25,6 +25,32 @@ public class Arrays_Playground {
         }
     }
     static void multiDimensionalArray(){//method for Multi-dimensional Array
+        int a[][] =new int[3][2];
+        a[0][0]=100;
+        a[0][1]=200;
+
+        a[1][0]=300;
+        a[1][1]=400;
+
+        a[2][0]=500;
+        a[2][1]=600;
+
+        int b[][] = {{100,900},{300,400},{500,600}}; //dynamic approach to creating an array
+        int number_rows= b.length;//number of rows
+        int columns = b[0].length;//number of columns [0] is used to find number of rows
+        //print all elements of array using for loop
+        for(int i=0;i<b.length;i++){// i represents rows
+            for(int j=0; j<b[i].length;j++){
+                System.out.println(b[i][j]);
+            }
+        }
+        //enhanced for loop
+        for(int r[]:b){//r[] stores the number of rows in the array
+            for(int i:r){ // variable i will get the column number after each run
+                System.out.println(i);
+
+            }
+        }
 
     }
     public static void main(String[] args) {
