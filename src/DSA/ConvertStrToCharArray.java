9209a136114934138be1342b12ddbb450eb2efcf
Aditya Kumar Rai <rai.aditya2k@gmail.com>
2023-01-16 09:41:49 +0530

    Convert string to charecter

---
+++ src/DSA/ConvertStrToCharArray.java
@@ -0,0 +1,23 @@
+package DSA;
+
+import static DSA.PrintDupliChar.input;
+
+public class ConvertStrToCharArray {
+    public static void main(String[] args){
+        String str =input();
+        char[] ch =new char[str.length()];
+        char[] ch1=new char[str.length()];
+        for(int i =0 ; i < str.length();i++){
+            ch[i] = str.charAt(i);
+        }
+        ch1=str.toCharArray();
+        for (int i =0; i< ch.length; i++) {
+            System.out.println(ch[i]);
+            ch[i]='0';
+        }for (int i =0; i< ch1.length; i++) {
+            System.out.println(ch1[i]);
+            ch1[i]='0';
+        }
+    }
+
+}
