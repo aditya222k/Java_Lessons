8dbcba7db17767a92941ce3887ddf232c1c052cc
Aditya Kumar Rai <rai.aditya2k@gmail.com>
2023-01-16 11:24:49 +0530

    count number of vowels and consonents

---
+++ src/DSA/CountConsonantsVowels.java
@@ -0,0 +1,34 @@
+package DSA;
+import java.util.ArrayList;
+
+import static DSA.PrintDupliChar.input;
+public class CountConsonantsVowels {
+
+    static ArrayList<Character> arrayListGenerator(String str){
+        ArrayList<Character> arrayList = new ArrayList<Character>();
+        for(int i=0;i<str.length();i++)
+        {
+            arrayList.add(str.charAt(i));
+        }
+        return arrayList;
+    }
+
+    static void counter(ArrayList<Character> vowels, String str ){
+        int vcount=0;
+        int ccount=0;
+
+        for(int i =0; i <str.length();i++){
+            if(vowels.contains(str.charAt(i))){
+                vcount++;
+            }else{
+                ccount++;
+            }
+        }
+        System.out.println("vcount = " + vcount);
+        System.out.println("ccount = " + ccount);
+    }
+    public static void main(String[] args){
+        String str= input();
+        counter(arrayListGenerator("aeiou"),str);
+    }
+}
