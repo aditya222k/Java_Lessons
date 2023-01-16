d0a0029a4062adf60c22a6ec184d9905213049e1
Aditya Kumar Rai <rai.aditya2k@gmail.com>
2023-01-16 15:15:49 +0530

    sum of pair equals target number

---
+++ src/DSA/ArrayPractice/SumOfPairs.java
@@ -0,0 +1,23 @@
+package DSA.ArrayPractice;
+
+import static DSA.ArrayPractice.DuplicateIntegersInArray.arrayInput;
+import static DSA.ArrayPractice.DuplicateIntegersInArray.in;
+
+public class SumOfPairs {
+    static void sumPair(int[] arr, int tar){
+        for(int i=0 ; i<arr.length;i++){
+            for(int j =i+1; j<arr.length;j++){
+                if(arr[i]+arr[j]==tar){
+                    System.out.println(arr[i]+","+arr[j]);
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        int n = in.nextInt();
+        int[] arr=arrayInput(n);
+        System.out.println("enter target");
+        int target= in.nextInt();
+        sumPair(arr,target);
+    }
+}
