bd72d07fcbb5af3c393560bd63d6a33fb61c2fa4
Aditya Kumar Rai <rai.aditya2k@gmail.com>
2023-01-18 09:25:29 +0530

    Bubble sort using recursion

--- src/DSA/SortingTechniques/BubbleSort.java
+++ src/DSA/SortingTechniques/BubbleSort.java
@@ -1,6 +1,6 @@
 package DSA.SortingTechniques;
-//TODO; complete using recursion as well
 /*
+https://www.geeksforgeeks.org/sorting-algorithms/
 iterates through the array till the number of swaps becomes 0. It checks the two neighbours and swaps them if the one is greater than others. The outer loop
 is to make sure that if there are no swaps in the inner loop it quits
 */
@@ -9,7 +9,6 @@ import java.util.Arrays;
 
 public class BubbleSort {
     static int[] bubbleSort(int[] arr) {
-        boolean flag=false;
         int n=arr.length;
         boolean swapped;
         for (int i=0; i < n - 1; i++)
@@ -31,8 +30,28 @@ public class BubbleSort {
         return arr;
     }
 
+    static void recursiveBubbleSort(int[] arr, int n){
+        int count =0;
+        for(int i=0; i<n-1; i++){
+            if(arr[i]>arr[i+1]){
+                int temp= arr[i];
+                arr[i]= arr[i+1];
+                arr[i+1]= temp;
+                count=count+1;
+            }
+
+        }
+            if(count==0){
+                return;
+            }
+            recursiveBubbleSort(arr,n-1);
+    }
+
     public static void main(String[] args) {
         int[] arr={64, 25, 12, 22, 11};
-        System.out.println(Arrays.toString(bubbleSort(arr)));
+        int n=arr.length;
+//        System.out.println(Arrays.toString(recursiveBubbleSort(arr,n)));
+        recursiveBubbleSort(arr,n);
+        System.out.println(Arrays.toString(arr));
     }
 }
