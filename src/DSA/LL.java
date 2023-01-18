088d6799c9174306872f4626525b9b3741fe9f08
Aditya Kumar Rai <rai.aditya2k@gmail.com>
2023-01-18 17:14:15 +0530

    LinkedList beginers

---
+++ src/DSA/LL.java
@@ -0,0 +1,19 @@
+package DSA;
+//https://www.youtube.com/watch?v=oAja8-Ulz6o
+
+public class LL {
+     Node head;
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data =data;
+            this.next=null;
+        }
+    }
+    public static void main(String[] args){
+     LL list=new LL();
+
+    }
+}
