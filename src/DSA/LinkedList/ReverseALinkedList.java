d6389150637c8678be2ab1fc5499680b0dbec18f
Aditya Kumar Rai <rai.aditya2k@gmail.com>
2023-01-18 22:57:11 +0530

    Reverse a linked list

---
+++ src/DSA/LinkedList/ReverseALinkedList.java
@@ -0,0 +1,12 @@
+package DSA.LinkedList;
+ import java.util.LinkedList;
+// https://www.youtube.com/watch?v=t7YaoQOFXzk
+
+public class ReverseALinkedList {
+
+    public static void main(String[] args){
+        LinkedList<String> list =new LinkedList<>();
+        list.peek();
+
+    }
+}
