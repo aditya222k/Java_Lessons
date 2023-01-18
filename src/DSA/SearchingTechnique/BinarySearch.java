360fd093b3898ec054ccef8077201146002efaa6
Aditya Kumar Rai <rai.aditya2k@gmail.com>
2023-01-18 16:27:50 +0530

    Binary Search

---
+++ src/DSA/SearchingTechnique/BinarySearch.java
@@ -0,0 +1,54 @@
+package DSA.SearchingTechnique;
+
+public class BinarySearch {
+    static void binarySearch(int[] arr, int tar) {
+        int l=0, h=arr.length - 1;
+        while (h - l > 0) {
+            int mid=(h - l) / 2;
+            if (arr[mid] == tar) {
+                System.out.println("found at " + arr[mid]);
+                break;
+            }
+            if (arr[mid] > tar) {
+                h=mid - 1;
+            } else if (arr[mid] < tar) {
+                l=mid + 1;
+            }
+        }
+        if (arr[l] == tar) {
+            System.out.println("found at " + arr[l]);
+        }
+        if (arr[h] == tar) {
+            System.out.println("found at" + arr[h]);
+        }
+    }
+
+    static int recursiveBinarySearch(int[] arr, int h, int l, int tar) {
+        if (h >= l) {
+            int mid=(h + l) / 2;
+
+            if (arr[mid] == tar) {
+                return mid;
+            } else if (arr[mid] > tar) {
+                h=mid - 1;
+                return recursiveBinarySearch(arr, h, l, tar);
+            } else if (arr[mid] < tar) {
+                l=mid + 1;
+                return recursiveBinarySearch(arr, h, l, tar);
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target=3;
+        int n=arr.length;
+//        binarySearch(arr, target);
+        int res=recursiveBinarySearch(arr, arr.length - 1, 0, 6);
+        if (res == -1)
+            System.out.println("element not found");
+        else
+            System.out.println("element found");
+    }
+}
