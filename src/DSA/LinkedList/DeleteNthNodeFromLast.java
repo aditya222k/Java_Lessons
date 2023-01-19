be4f1fd320ee4ad6609c36a3a393007035fa9523
Aditya Kumar Rai <rai.aditya2k@gmail.com>
2023-01-19 11:03:30 +0530

    Delete nth element from last

--- src/DSA/LinkedList/DeleteNthNodeFromLast.java
+++ src/DSA/LinkedList/DeleteNthNodeFromLast.java
@@ -1,8 +1,41 @@
 package DSA.LinkedList;
-import DSA.LinkedList.ReverseALinkedList;
-
+//https://www.youtube.com/watch?v=cL4gHVuFOvk
 public class DeleteNthNodeFromLast {
-    public static void main(String[] args){
+
+    public static ReverseALinkedList.Node removeNthNodeFromEnd(ReverseALinkedList.Node head, int n) {
+        if (head.next == null) {
+            return null;
+        }
+        int size=0;
+        ReverseALinkedList.Node curr=head;
+        while (curr != null) {//Finding the size of the list
+            curr=curr.next;
+            size++;
+            System.out.println(size);
+        }
+
+        if(n==size){
+            return head.next;
+        }
+
+        int indexSearch = size-n;// to find the position of the value of the node to be deleted
+        ReverseALinkedList.Node prev= head;
+        int i=0;
+        while (i<indexSearch){//go to the element to be searched-1
+            prev=prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;//skipping a node in the middle to directly jump the next
+        return head;
+    }
+    public static void main(String[] args) {
+        ReverseALinkedList list=new ReverseALinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        removeNthNodeFromEnd(list.head,2);
+        list.printList();
 
     }
 }
